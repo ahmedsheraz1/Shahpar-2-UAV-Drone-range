52259e627ece8b458d18a3774854d2da3ce59db5
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-05 03:48:31 +0500

    Create Shahpar-2 Drone range.py

---
+++ Shahpar-2 Drone range.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from math import gamma
+gamma(7.2)
+1050.317816662683
